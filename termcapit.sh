1b6523fa56b61e50d00c8e5b4f012e75fb2fac01
Ish West <ishwest.yt@yandex.ru>
2017-07-27 18:51:18 +0200

    Now it works on a mac

--- termcapit.sh
+++ termcapit.sh
@@ -16,7 +16,7 @@ function add_to_cleanable {
 }
 
 function cleanup {
-	rm -rf $_CLEANABLE
+	: # rm -rf $_CLEANABLE
 }
 
 function brag_and_exit {
@@ -34,13 +34,26 @@ function brag_and_exit {
 	exit 1
 }
 
+read -r -d '' SED_JOIN << EOSJ
+: repeat
+/\\\\$/ {
+	N
+	s/\\\\\n[[:blank:]]*/@NEWLINE@/
+	t repeat
+}
+EOSJ
+
+read -r -d '' SED_SPLIT << EOSS
+s/@NEWLINE@/\\\\\\
+	/g
+EOSS
 
 UPDATE="$1"
 [ -f "$UPDATE" ] || brag_and_exit "Update file not found"
 
 SOURCE="${2-/etc/termcap}"
 
-COMPILED_TMP="$(mktemp -p /tmp ${0##*/}_compiled.XXXXXXXXXX)" || brag_and_exit "Can not create temp file for compiling"
+COMPILED_TMP="$(mktemp /tmp/${0##*/}_compiled.XXXXXXXXXX)" || brag_and_exit "Can not create temp file for compiling"
 add_to_cleanable "$COMPILED_TMP"
 
 if [ -f "$SOURCE" ] ; then
@@ -48,10 +61,10 @@ if [ -f "$SOURCE" ] ; then
 
 	BACKUP_NEEDED=YES
 
-	JOINED_TMP="$(mktemp -p /tmp ${0##*/}_joined.XXXXXXXXXX)" || brag_and_exit "Can not create temp file for joining"
+	JOINED_TMP="$(mktemp /tmp/${0##*/}_joined.XXXXXXXXXX)" || brag_and_exit "Can not create temp file for joining"
 	add_to_cleanable "$JOINED_TMP"
 
-	sed ':x; /\\$/ { N; s/\\\n[\t]*/@NEWLINE@/; tx }' < "$SOURCE" >> "$JOINED_TMP"
+	sed -E -e "$SED_JOIN" < "$SOURCE" >> "$JOINED_TMP"
 
 	# Compose the list of entry names to be updated
 	while IFS='' read -r LINE ; do
@@ -61,29 +74,24 @@ if [ -f "$SOURCE" ] ; then
 		# Loose description, escape and add to the list of potentially overlapping names
 		OVERLAP_NAMES="${OVERLAP_NAMES}$(printf '%s%q' "${OVERLAP_NAMES:+|}" "${LINE%|*}")" 
 	done < "$UPDATE"
-	
-	grep -vE "^($OVERLAP_NAMES)[:|]" < "$JOINED_TMP" | sed 's/@NEWLINE@/\\\n\t/g' >> "$COMPILED_TMP"
+
+	grep -vE "^($OVERLAP_NAMES)[:|]" < "$JOINED_TMP" | sed -e "$SED_SPLIT" >> "$COMPILED_TMP"
 elif [ ! -e "$SOURCE" ] ; then
 	touch "$SOURCE" 2>/dev/null || brag_and_exit "Can not create termcap file $SOURCE"
 else
 	brag_and_exit "Not a termcap file $SOURCE"
 fi
 
-ls -l "$UPDATE"
-ls -l "$COMPILED_TMP"
 cat "$UPDATE" >> "$COMPILED_TMP" 
-ls -l "$COMPILED_TMP"
 
 if [ "$BACKUP_NEEDED" = 'YES' ] ; then 
 	BACKUP="$SOURCE.$(date +%Y-%m-%d-%H-%M-%S).backup"
 	cp -a "$SOURCE" "$BACKUP" || brag_and_exit "Can not backup $SOURCE to $BACKUP"
 fi
 
-ls -l "$SOURCE"
 (
 	cat "$COMPILED_TMP" > "$SOURCE" || brag_and_exit "Can not write updated termcap to $SOURCE"
 ) 2>/dev/null; 
-ls -l "$SOURCE"
 
 
 cleanup
--- tst/runtests.sh
+++ tst/runtests.sh
@@ -7,15 +7,15 @@ function report_test_result {
 	local test_exit_code="$2"
 
 	if [ "$test_exit_code" -eq 0 ] ; then
-		test_ok=Ok
+		test_ok=Pass
 	else
-		test_ok=Error
+		test_ok=Fail
 	fi
 
 	echo "$test_name :: $test_ok"
 }
 
-[ "$(id -u)" -lt 1000 ] && echo 'Should run as pedestrian user, NOT as root' && exit 1
+[ "$(id -u)" -lt 500 ] && echo 'Should run as pedestrian user, NOT as root' && exit 1
 sudo -v || exit 1
 
 ALL_OK=0
@@ -26,14 +26,14 @@ report_test_result "$TEST_NAME" "$EXIT_CODE"
 [ "$EXIT_CODE" -eq 0 ] || ALL_OK=1
 
 TEST_NAME='Source is dir -> Error'
-TST_DIR="$(mktemp -p /tmp -d ${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
+TST_DIR="$(mktemp -d /tmp/${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
 "$TESTED" screen-termcap "$TST_DIR" >/dev/null 2>/dev/null ;  [ "$?" -ne 0 ] ; EXIT_CODE="$?"
 report_test_result "$TEST_NAME" "$EXIT_CODE"
 [ "$EXIT_CODE" -eq 0 ] || ALL_OK=1
 rmdir "$TST_DIR"
 
 TEST_NAME='Source not writable -> Error'
-TST_SOURCE="$(sudo -n mktemp -p /tmp ${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
+TST_SOURCE="$(sudo -n mktemp /tmp/${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
 cat dsm-termcap | sudo -n tee -a "$TST_SOURCE" >/dev/null
 "$TESTED" screen-termcap "$TST_SOURCE" >/dev/null 2>/dev/null ;  [ "$?" -ne 0 ] ; EXIT_CODE="$?"
 report_test_result "$TEST_NAME" "$EXIT_CODE"
@@ -41,14 +41,14 @@ report_test_result "$TEST_NAME" "$EXIT_CODE"
 sudo -n rm "$TST_SOURCE"
 
 TEST_NAME='No source in a non writable dir -> Error'
-TST_DIR="$(sudo -n mktemp -p /tmp -d ${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
+TST_DIR="$(sudo -n mktemp -d /tmp/${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
 "$TESTED" screen-termcap "$TST_DIR"/termcap >/dev/null 2>/dev/null ;  [ "$?" -ne 0 ] ; EXIT_CODE="$?"
 report_test_result "$TEST_NAME" "$EXIT_CODE"
 [ "$EXIT_CODE" -eq 0 ] || ALL_OK=1
 sudo -n rmdir "$TST_DIR"
 
 TEST_NAME='Source does nor exist -> result == update & no backup'
-TST_SOURCE="$(mktemp -p /tmp ${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
+TST_SOURCE="$(mktemp /tmp/${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
 rm "$TST_SOURCE"
 BACKUP="$TST_SOURCE.????-??-??-??-??-??.backup"
 "$TESTED" screen-termcap "$TST_SOURCE" >/dev/null 2>/dev/null ; EXIT_CODE="$?"
@@ -63,7 +63,7 @@ report_test_result "$TEST_NAME" "$EXIT_CODE"
 rm "$TST_SOURCE"
 
 TEST_NAME='Source and update do not overlap -> result == source . update & backup'
-TST_SOURCE="$(mktemp -p /tmp ${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
+TST_SOURCE="$(mktemp /tmp/${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
 cat dsm-termcap >> "$TST_SOURCE"
 BACKUP="$TST_SOURCE.????-??-??-??-??-??.backup"
 "$TESTED" screen-termcap "$TST_SOURCE" >/dev/null 2>/dev/null ; EXIT_CODE="$?"
@@ -81,7 +81,7 @@ report_test_result "$TEST_NAME" "$EXIT_CODE"
 rm "$TST_SOURCE" $BACKUP
 
 TEST_NAME='Source and update DO overlap -> result == source - update .update & backup'
-TST_SOURCE="$(mktemp -p /tmp ${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
+TST_SOURCE="$(mktemp /tmp/${0##*/}_${TESTED##*/}.XXXXXXXXXX)"
 cat dsm-termcap >> "$TST_SOURCE"
 BACKUP="$TST_SOURCE.????-??-??-??-??-??.backup"
 "$TESTED" dsm-termcap-extract "$TST_SOURCE" >/dev/null 2>/dev/null ; EXIT_CODE="$?"
